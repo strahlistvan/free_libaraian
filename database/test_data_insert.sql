8751f3d771e67c9e755b225aba48e97ba817f5a9
Stráhl István <strahlistvan0@gmail.com>
2017-01-15 11:33:12 +0100

    Repair test insert script

--- database/test_data_insert.sql
+++ database/test_data_insert.sql
@@ -2,27 +2,78 @@ INSERT INTO lib_t_user(user_id,
                        name,
                        email,
                        birthdate,
-                       id_number,
-                       is_deleted)
+                       id_number )
 VALUES (1,
         'Teszt Elek',
         'teszt@elek.hu',
         ADD_MONTHS(SYSDATE, -240),
-        '112233AA',
-        0);     
-   
+        '112233AA' );     
+
+-- Add ISBN number as a document identifier   
 INSERT INTO lib_t_doc_identifier_type(id,
                                       name,
                                       format)
 VALUES (1, 'ISBN', '/\d{13}/');
-
+ 
+-- New ISBN numbers
 INSERT INTO lib_t_doc_identifier(id,
                                  doc_identifier_type_id,
                                  content)
 VALUES (1,
         1,
         '1234567890000');
-                             
+
+INSERT INTO lib_t_doc_identifier(id,
+                                 doc_identifier_type_id,
+                                 content)
+VALUES (2,
+        1,
+        '1234567890001');
+				
+INSERT INTO lib_t_doc_identifier(id,
+                                 doc_identifier_type_id,
+                                 content)
+VALUES (3,
+        1,
+        '1234567890002');								
+				
+-- New documents (with ISBN)				
+INSERT INTO lib_t_document(instance_id,
+                           doc_type_id,
+                           doc_identifier_id,
+                           is_borrowable)
+values(1, 1, 1, 1);
+
+INSERT INTO lib_t_document(instance_id,
+                           doc_type_id,
+                           doc_identifier_id,
+                           is_borrowable)
+values(2, 1, 1, 1);
+
+INSERT INTO lib_t_document(instance_id,
+                           doc_type_id,
+                           doc_identifier_id,
+                           is_borrowable)
+values(3, 1, 1, 1);			
+	
+-- New books     
+INSERT INTO lib_t_book( instance_id,
+                       author,
+                       title,
+                       pages,
+                       publish_year,
+                       short_description,
+                       eto_number,
+                       cutter_number)        
+VALUES ( 1,
+         'Gárdonyi Géza',
+         'A láthatatlan ember',
+         350,
+         2001,
+         'Vannak benne hunok meg rómaiak is',
+         NULL,
+         'G 11' );
+                       
 INSERT INTO lib_t_book( instance_id,
                        author,
                        title,
@@ -40,13 +91,6 @@ VALUES ( 2,
          NULL,
          'G 11' );
         
-INSERT INTO lib_t_document(instance_id,
-                           doc_type_id,
-                           doc_identifier_id,
-                           is_borrowable)
-VALUES(2, 1, 1, 1);
-
-
 INSERT INTO lib_t_book(instance_id,
                        author,
                        title,
@@ -63,13 +107,6 @@ VALUES ( 3,
          'Vannak benne vörösingesek.',
          NULL,
          'G 11' );
-        
-INSERT INTO lib_t_document(instance_id,
-                           doc_type_id,
-                           doc_identifier_id,
-                           is_borrowable)
-VALUES(3, 1, 1, 1);
-
 
 COMMIT;
 
