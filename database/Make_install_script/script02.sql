5b6d780c5000d91b323c7d596245f4ee4cf59213
strahlistvan <strahlistvan@users.noreply.github.com>
2017-02-10 22:36:11 +0100

    Web application skeleton (Eclipse project)

---
+++ database/Make_install_script/script02.sql
@@ -0,0 +1,95 @@
+-- Create sequences
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_borrowing.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_borrowing.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_document.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_document.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_doc_identifier.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_doc_identifier.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_doc_identifier_type.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_doc_identifier_type.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_errors.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_errors.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_trace_logs.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_trace_logs.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_user.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\sequences\lib_sq_user.sql
+-- Create tables
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\book\lib_ht_book.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\book\lib_ht_book.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\book\lib_t_book.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\book\lib_t_book.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\borrowing\lib_ht_borrowing.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\borrowing\lib_ht_borrowing.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\borrowing\lib_t_borrowing.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\borrowing\lib_t_borrowing.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\document\lib_ht_document.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\document\lib_ht_document.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\document\lib_t_document.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\document\lib_t_document.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\doc_identifier\lib_ht_doc_identifier.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\doc_identifier\lib_ht_doc_identifier.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\doc_identifier\lib_t_doc_identifier.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\doc_identifier\lib_t_doc_identifier.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\doc_identifier_type\lib_ht_doc_identifier_type.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\doc_identifier_type\lib_ht_doc_identifier_type.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\doc_identifier_type\lib_t_doc_identifier_type.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\doc_identifier_type\lib_t_doc_identifier_type.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\technical\lib_t_errors.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\technical\lib_t_errors.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\technical\lib_t_logs.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\technical\lib_t_logs.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\user\lib_ht_user.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\user\lib_ht_user.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\user\lib_t_user.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\tables\user\lib_t_user.sql
+-- Create foreign_keys
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_book_document.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_book_document.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_book_doc_identifier_b.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_book_doc_identifier_b.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_document_borrowing.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_document_borrowing.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_doc_identifier_type_i.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_doc_identifier_type_i.sql
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_user_borrowing.sql === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\foreign_keys\lib_fk_user_borrowing.sql
+-- Create triggers
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\book\lib_htr_book.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\book\lib_htr_book.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\book\lib_pktr_book.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\book\lib_pktr_book.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\borrowing\lib_htr_borrowing.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\borrowing\lib_htr_borrowing.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\borrowing\lib_pktr_borrowing.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\borrowing\lib_pktr_borrowing.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\document\lib_htr_document.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\document\lib_htr_document.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\document\lib_pktr_document.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\document\lib_pktr_document.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\doc_identifier\lib_htr_doc_identifier.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\doc_identifier\lib_htr_doc_identifier.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\doc_identifier\lib_pktr_doc_identifier.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\doc_identifier\lib_pktr_doc_identifier.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\doc_ident_type\lib_htr_doc_ident_type.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\doc_ident_type\lib_htr_doc_ident_type.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\doc_ident_type\lib_pktr_doc_ident_type.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\doc_ident_type\lib_pktr_doc_ident_type.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\user\lib_htr_user.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\user\lib_htr_user.trg
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\user\lib_pktr_user.trg === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\triggers\user\lib_pktr_user.trg
+-- Create procedure
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\procedures\lib_prc_log_errors.prc === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\procedures\lib_prc_log_errors.prc
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\procedures\lib_prc_log_trace.prc === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\procedures\lib_prc_log_trace.prc
+-- Create package
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\packages\lib_pck_surcharge.spc === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\packages\lib_pck_surcharge.spc
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\packages\lib_pck_user_action.spc === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\packages\lib_pck_user_action.spc
+-- Create package body
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\packages\lib_pck_surcharge.bdy === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\packages\lib_pck_surcharge.bdy
+prompt === Create C:\Users\Istvan\Documents\PROGI\free_librarian\database\packages\lib_pck_user_action.bdy === 
+@C:\Users\Istvan\Documents\PROGI\free_librarian\database\packages\lib_pck_user_action.bdy
